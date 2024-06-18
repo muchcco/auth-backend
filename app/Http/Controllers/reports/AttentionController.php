da6e7c94312718586374a8a052df38f4bb2b38df
kevinmuchcco <kevinmuchcco@gmail.com>
2024-06-18 11:50:40 -0500

    demora de carrga

--- app/Http/Controllers/reports/AttentionController.php
+++ app/Http/Controllers/reports/AttentionController.php
@@ -27,59 +27,68 @@ class AttentionController extends Controller
 
     public function tableAttention(Request $request)
     {
-        $data = DB::connection('sqlsrv')->table('cre_atend as c')
-                                            ->join('par_ent as p', 'c.Ide_ser', '=', 'p.ide_ent')
-                                            ->select(
-                                                'c.Nom_mac',
-                                                'p.nom_ent',
-                                                'c.Hra_llg',
-                                                'c.Hra_lla',
-                                                'c.Tpo_esp',
-                                                'c.Hra_ini',
-                                                'c.Tpo_ate',
-                                                'c.Fin_ate',
-                                                'c.Tpo_tot',
-                                                'c.Num_tik',
-                                                'c.Est_ate',
-                                                'c.Fec_ate',
-                                                DB::raw("'Presencial' as presencial"),
-                                                DB::raw("CASE c.des_pri
-                                                            WHEN '1' THEN 'NO PREFERENCIAL'
-                                                            ELSE 'PREFERENCIAL'
-                                                        END as TIPO_aTE")
-                                            )
-                                            ->where(function($query) use ($request) {                                
-                                                $fecha_I = date("Y-m-d");
-                                                $fecha_F = date("Y-m-d");
-                                                if($request->fecha_inicio != '' && $request->fecha_fin != '' ){
-                                                    $query->where('fec_ate', '>=', $request->fecha_inicio);
-                                                    $query->where('fec_ate', '<=', $request->fecha_fin);
-                                                }else{
-                                                    $query->where('fec_ate', '<=', $fecha_I);
-                                                    $query->where('fec_ate', '>=', $fecha_F);
-                                                }
-                                            })
-                                            ->where(function($query) use ($request) {
-                                                if($request->servicio != '' ){
-                                                    $query->where('Ide_ser', $request->servicio);
-                                                }
-                                            })
-                                            // ->whereIn('Nom_mac', ['MAC Callao', 'MAC Ventanilla'])
-                                            ->where(function($query) use ($request) {
-                                                if($request->nom_mac == "0" ){
-                                                    $query->whereIn('Nom_mac' , [$request->nom_mac]);
-                                                }
-                                            })
-                                            ->orderBy('Nom_mac')
-                                            ->orderBy('Fec_ate')
-                                            ->get();
+        try {
+            $data = DB::connection('sqlsrv')->table('cre_atend as c')
+                                                ->join('par_ent as p', 'c.Ide_ser', '=', 'p.ide_ent')
+                                                ->select(
+                                                    'c.Nom_mac',
+                                                    'p.nom_ent',
+                                                    'c.Hra_llg',
+                                                    'c.Hra_lla',
+                                                    'c.Tpo_esp',
+                                                    'c.Hra_ini',
+                                                    'c.Tpo_ate',
+                                                    'c.Fin_ate',
+                                                    'c.Tpo_tot',
+                                                    'c.Num_tik',
+                                                    'c.Est_ate',
+                                                    'c.Fec_ate',
+                                                    DB::raw("'Presencial' as presencial"),
+                                                    DB::raw("CASE c.des_pri
+                                                                WHEN '1' THEN 'NO PREFERENCIAL'
+                                                                ELSE 'PREFERENCIAL'
+                                                            END as TIPO_aTE")
+                                                )
+                                                ->where(function($query) use ($request) {                                
+                                                    $fecha_I = date("Y-m-d");
+                                                    $fecha_F = date("Y-m-d");
+                                                    if($request->fecha_inicio != '' && $request->fecha_fin != '' ){
+                                                        $query->where('fec_ate', '>=', $request->fecha_inicio);
+                                                        $query->where('fec_ate', '<=', $request->fecha_fin);
+                                                    }else{
+                                                        $query->where('fec_ate', '<=', $fecha_I);
+                                                        $query->where('fec_ate', '>=', $fecha_F);
+                                                    }
+                                                })
+                                                ->where(function($query) use ($request) {
+                                                    if($request->servicio != '' ){
+                                                        $query->where('Ide_ser', $request->servicio);
+                                                    }
+                                                })
+                                                // ->whereIn('Nom_mac', ['MAC Callao', 'MAC Ventanilla'])
+                                                ->where(function($query) use ($request) {
+                                                    if($request->nom_mac == "0" ){
+                                                        $query->whereIn('Nom_mac' , [$request->nom_mac]);
+                                                    }
+                                                })
+                                                ->orderBy('Nom_mac')
+                                                ->orderBy('Fec_ate')
+                                                ->get();
 
-        // dd($data);
-        
-        return response()->json([
-            "status" => true,
-            "message" => "Lista de usuarios",
-            "data" => $data
-        ]);
+            // dd($data);
+            
+            return response()->json([
+                "status" => true,
+                "message" => "Lista de usuarios",
+                "data" => $data
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => "Se excedió el tiempo de carga. Inténtelo de nuevo más tarde.",
+                "error" => $e->getMessage()
+            ], 500);
+        }
     }
 }

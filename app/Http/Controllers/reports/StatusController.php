31e1f08802d10a2da183b0b007960aab822e42c4
kevinmuchcco <kevinmuchcco@gmail.com>
2024-06-18 16:51:09 -0500

    añadir carbon a status controller

--- app/Http/Controllers/reports/StatusController.php
+++ app/Http/Controllers/reports/StatusController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\reports;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 
 class StatusController extends Controller
